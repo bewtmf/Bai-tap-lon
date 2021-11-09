d510281477c20f44fe969a2067f9160f04643b85
tanhluong <82999712+tanhluong@users.noreply.github.com>
2021-11-09 12:45:37 +0700

    Person (Vacxin)

--- Person.cpp
+++ Person.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 class Person
 {
 protected:
     string name, address;
-    int age, mui1, mui2;
+    int age, mui1, mui2, Tenvx;
 
 public:
     virtual void Nhap();
@@ -21,14 +20,28 @@ void Person::Nhap()
     cout << "Nhap ho ten: ";
     getline(cin, name);
     cout << "Nhap noi o hien tai: ";
-    getline(cin, address);
+    getline(cin,address);
     do
     {
-        cout << "Tuoi:";
-        cin >> age;
-        if (age < 18 || age > 150)
-            cout << "Khong hop le de tiem va vui lop nhap lai:" << endl;
-    } while (age < 18 || age > 150);
+    	cout << "Tuoi:";
+    	cin >> age;
+    	if(age < 12 || age > 150)
+    		cout << "Khong hop le de tiem va vui lop nhap lai:" << endl;
+	}
+	while (age < 12 || age > 150);
+	
+	    fflush(stdin);
+    cout << "*****Nhap 0 neu tiem Pfizer(USA) va nhap 1 neu tiem Verocell(TQ)*****" << endl;
+    do{
+	
+    cout << "Nhap ten thuoc tiem Vaccine:" ;
+    cin>>Tenvx;
+    if (Tenvx < 0 || Tenvx > 1 )
+    	cout << "Khong hop le vui long nhap lai!" << endl;
+	}
+	while (Tenvx < 0 || Tenvx >1);
+	
+	cout << "*****Nhap 0 neu chua tiem va nhap 1 neu da tiem!*****" << endl;
     do
     {
         cout << "Mui 1: ";
@@ -36,7 +49,7 @@ void Person::Nhap()
         if (mui1 < 0 || mui1 > 1)
             cout << "Khong hop le va vui long nhap lai" << endl;
     } while (mui1 < 0 || mui1 > 1);
-
+	
     if (mui1 == 1)
     {
         do
@@ -54,18 +67,23 @@ void Person::Nhap()
 void Person::Xuat()
 {
     fflush(stdin);
-    cout << "Ho ten: " << name << endl;
-    cout << "Noi o hien tai: " << address << endl;
-    cout << "So tuoi:" << age << endl;
-    cout << "************Tinh trang tiem chung********" << endl;
-    cout << "Mui 1: ";
+    cout << "			-> Ho ten:" << name << endl;
+    cout << "			-> Noi o hien tai:" << address << endl;
+    cout << "			-> So tuoi:" << age << endl;
+    cout << "			-> Ten vacxin:";
+    if (Tenvx == 0)
+    	cout << "Pfizer(USA) co muc do hieu luc la 95%." << endl;
+    else if
+    	(Tenvx == 1)
+    	cout <<"Verocell(TQ)co muc do hieu luc la 79%." << endl;
+    cout << "			-> Mui 1:";
     if (mui1 == 0)
-        cout << "chua tiem" << endl;
+        cout << "chua tiem." << endl;
     else if (mui1 == 1)
-        cout << "da tiem" << endl;
-    cout << "Mui 2: ";
+        cout << "da tiem." << endl;
+    cout << "			-> Mui 2:";
     if (mui2 == 0)
-        cout << "chua tiem" << endl;
+        cout << "chua tiem." << endl;
     else if (mui2 == 1)
-        cout << "da tiem" << endl;
+        cout << "da tiem." << endl;
 }
